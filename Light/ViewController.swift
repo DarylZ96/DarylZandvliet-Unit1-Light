e2ef61e30e2ef0be42a2f21f9627268c84a6ac9c
Daryl Zandvliet <Daryl@MacBook-Pro-van-Daryl.local>
2018-11-01 15:23:53 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Daryl Zandvliet on 01/11/2018.
+//  Copyright © 2018 Daryl Zandvliet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
